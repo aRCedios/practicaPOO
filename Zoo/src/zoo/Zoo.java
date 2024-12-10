81be9046afd6be9cf02aef6938ffd48c9ddea65b
aRCe <>
2024-12-09 19:57:41 -0500

    fin?

--- Zoo/src/zoo/Zoo.java
+++ Zoo/src/zoo/Zoo.java
@@ -3,6 +3,27 @@ package zoo;
 import javax.swing.JOptionPane;
 public class Zoo {
     public static void main(String[] args) {
-        String nombre = JOptionPane.showInputDialog("");
+        String nombreP = JOptionPane.showInputDialog("Ingrese el nombre de un cuidador: ");
+        String idP = JOptionPane.showInputDialog("Ingrese el numero de identificación del cuidador: ");
+        int edadP = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la edad de la persona: "));
+        Cuidadores cuidador1 = new Cuidadores(nombreP,idP,edadP);
+        String nombreA = JOptionPane.showInputDialog("Ingrese el nombre de un animal: ");
+        String idA = JOptionPane.showInputDialog("Ingrese el numero de identificación del animal: ");
+        int edadA = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la edad de su animal: "));
+        String especie = JOptionPane.showInputDialog("Ingrese la especie de su animal: ");
+        Animales animal1 = new Animales(nombreA,idA,edadA,especie);
+        JOptionPane.showMessageDialog(null, "Información cuidador: "+cuidador1.datos());
+        JOptionPane.showMessageDialog(null, "Información animal: "+animal1.datos());
+        JOptionPane.showMessageDialog(null, "El animalito se encuentra triste, seguramente tiene hambre, su casa está sucia o quiere jugar, pongamos al cuidador a trabajar.");
+        cuidador1.darComida(animal1);
+        JOptionPane.showMessageDialog(null, "Información animal: "+animal1.datos());
+        cuidador1.limpiarCasa(animal1);
+        JOptionPane.showMessageDialog(null, "Información animal: "+animal1.datos());
+        cuidador1.jugar(animal1);
+        JOptionPane.showMessageDialog(null, "Información animal: "+animal1.datos());
+        JOptionPane.showMessageDialog(null, "El cuidador ha terminado su trabajo y se va a dormir. ");
+        
+        
+        
     }
 }
